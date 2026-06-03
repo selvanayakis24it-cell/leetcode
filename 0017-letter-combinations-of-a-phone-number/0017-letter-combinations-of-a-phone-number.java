553c4be236a2b79553bdfaca7cbf89a97727573a
Selvanayaki S <selvanayakis.24it@kongu.edu>
2026-06-03 06:26:14 +0530

    Time: 3 ms (33.34%), Space: 49.2 MB (10.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,20 +4,20 @@ class Solution {
         if(digits.length()==0){
             return ans;
         }
-        backtrack("",digits,ans,0);
-        return ans;
+  backtrack("",digits,ans,0);
+  return ans;
     }
-    public static void backtrack(String curr,String digits,List<String> ans,int index){
+    public static void  backtrack(String curr,String digits,List<String> ans,int index){
         String[] map={"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         if(index==digits.length()){
             ans.add(curr);
             return;
         }
-        String letters=map[digits.charAt(index)-'2'];
-        for(char ch:letters.toCharArray()){
+        String letter=map[digits.charAt(index)-'2'];
+        for(char ch:letter.toCharArray()){
             String next=curr+ch;
             backtrack(next,digits,ans,index+1);
-
         }
+
     }
 }
