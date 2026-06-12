a815f5a264f9d03a63db745510eb8d7e76525ee8
Selvanayaki S <selvanayakis.24it@kongu.edu>
2026-06-12 11:16:48 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,14 +3,14 @@ class Solution {
         if(n<1){
             return n;
         }
-        int count=0;
+//space optimization
+
         int prev1=1;
         int prev2=0;
         for(int i=0;i<n;i++){
             int curr=prev1+prev2;
             prev2=prev1;
             prev1=curr;
-            count++;
         }
         return prev1;
     }
