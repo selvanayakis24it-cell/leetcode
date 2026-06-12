f6838cbf3d37159b155c27649cec2b7273fae89a
Selvanayaki S <selvanayakis.24it@kongu.edu>
2026-06-12 10:59:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (32.88%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        int[] dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       if(n<2){
+        return n;
+       } 
+       int prev1=1;
+       int prev2=0;
+       for(int i=2;i<=n;i++){
+        int curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+       }
+       return prev1;
     }
 }
+//spla
