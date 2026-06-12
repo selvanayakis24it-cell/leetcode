7e28a52267eae2dfc157e961ff9e5680f777a33a
Selvanayaki S <selvanayakis.24it@kongu.edu>
2026-06-12 11:29:22 +0530

    Time: 0 ms (100%), Space: 42 MB (52.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,18 +4,12 @@ class Solution {
             return n;
         }
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fun(n,dp);
-    }
-    public int fun(int n,int[] dp){
-        if(n<=2){
-            return n;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int res=fun(n-1,dp)+fun(n-2,dp);
-        dp[n]=res;
-        return res;
+        return dp[n];
     }
-}//memoization method
+}
