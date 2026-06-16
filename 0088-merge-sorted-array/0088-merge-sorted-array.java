0774bc620860d046dc7d10385ef07f12cf53be48
Selvanayaki S <selvanayakis.24it@kongu.edu>
2026-06-16 11:54:13 +0530

    Time: 0 ms (100%), Space: 43.9 MB (27.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,35 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int last=m-1;
-        int first=0;
-        while(last>0  && first<n ){
-            if(nums1[last]>nums2[first]){
-                int temp=nums1[last];
-                nums1[last]=nums2[first];
-                nums2[first]=temp;
-
-                int k=0;
-                while(k+1>n && nums2[k]>nums2[k+1]){
-                    int swap=nums2[k];
-                    nums2[k]=nums2[k+1];
-                    nums2[k+1]=swap;
-                    k++;
-                }
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int j=0;
+//         for(int i=m;i<m+n;i++){
+//             nums1[i]=nums2[j++];
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }
+//has O((n+m)log(n+m))
+//need to optimize the solution using 3 pointers  we are using  nums1 la oru pointer adhu m-1 and nums2 la oru pointer n-1 aproo n+m-1 nu onu vekkuro num1 la edhukkuna naga thana namma second array la irukka elements a kondu vandhu add panna porom
+class Solution{
+    public void merge(int[] nums1,int m,int[] nums2,int n){
+        int ptr1=m-1;
+        int ptr2=n-1;
+        int writer=n+m-1;
+        while(ptr1>=0 && ptr2>=0){
+            if(nums1[ptr1]<nums2[ptr2]){
+                nums1[writer]=nums2[ptr2];
+                writer--;
+                ptr2--;
             }
-            last--;
+            else{
+                nums1[writer]=nums1[ptr1];
+                writer--;
+                ptr1--;
+            }
         }
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
+        while(ptr2>=0){
+            nums1[writer]=nums2[ptr2];
+            ptr2--;
+            writer--;
         }
-        Arrays.sort(nums1);
     }
 }
